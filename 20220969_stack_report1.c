7cfd2a361a2901433516e79350995cfe2ce3991c
20220969-KimKiHwan <20220969@vision.hoseo.edu>
2026-04-05 17:45:11 +0900

    Update 20220969_stack_report1.c

--- 20220969_stack_report1.c
+++ 20220969_stack_report1.c
@@ -1,38 +1,19 @@
 int main() {
-    char *str;
-    char *stack;
-    int top = -1;
-    int i = 0;
-
-    str = (char *)malloc(sizeof(char) * 101);
-    if (str == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-
-    printf("문자열을 입력하세요: ");
-    fgets(str, 101, stdin);
-
-    stack = (char *)malloc(sizeof(char) * 101);
-    if (stack == NULL) {
-        printf("Memory allocation failed\n");
-        free(str);
-        return 1;
-    }
-
-    while (str[i] != '\0' && str[i] != '\n') {
-        stack[++top] = str[i];
-        i++;
-    }
-
-    printf("거꾸로 출력된 문자열: ");
-    while (top != -1) {
-        printf("%c", stack[top--]);
+    Stack s;
+    char str[MAX]; 
+    initStack(&s);
+    
+    printf("문자열 입력: ");
+    fgets(str,MAX, stdin);
+    str[strcspn(str, "\n")] = '\0';
+    
+    for(int i=0;str[i] != '\0';i++)     push(&s, str[i]);
+    printf("거꾸로 된 문자열: ");
+    while(!isEmpty(&s)){
+        printf("%c", pop(&s));
     }
     printf("\n");
-
-    free(str);
-    free(stack);
-
+    
     return 0;
+    
 }
